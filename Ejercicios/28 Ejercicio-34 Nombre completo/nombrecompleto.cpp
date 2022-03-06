01e15a9918b6ad1077e891322759931335f470ff
jorgevalladares <albertovr100@hotmail.com>
2022-03-06 12:06:44 -0600

    agregando ejercicio nombre completo

---
+++ Ejercicios/28 Ejercicio-34 Nombre completo/nombrecompleto.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+string nombrecompleto (string nombre, string apellido)
+{
+    return nombre + " " + apellido;
+}
+
+int main(int argc, char const *argv[])
+{
+    string primerNombre = "";    
+    string primerApellido = "";
+    
+    cout << "ingrese su primer nombre: ";
+    cin >> primerNombre;
+    cout << "ingrese su primer apellido: ";
+    cin >> primerApellido;
+
+    cout << endl;
+    cout << "nombre completo: " << nombrecompleto(primerNombre, primerApellido);
+    return 0;
+}
