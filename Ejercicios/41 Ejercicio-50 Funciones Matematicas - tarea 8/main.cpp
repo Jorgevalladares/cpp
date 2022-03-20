bf5d7ea0f9250b6b96ab2d125a463602af4dfb8a
jorgevalladares <albertovr100@hotmail.com>
2022-03-19 20:50:48 -0600

    agregando tarea 8

---
+++ Ejercicios/41 Ejercicio-50 Funciones Matematicas - tarea 8/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+   double numero = 253;
+   double raizcuadrada = sqrt(numero);
+
+   cout << "la raiz cuadrada de: " << numero << endl << raizcuadrada;
+
+    
+
+    return 0;
+}
