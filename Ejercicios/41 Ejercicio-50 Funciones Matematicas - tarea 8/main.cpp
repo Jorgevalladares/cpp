bd3c773e71316452f26c66b12dd3a8844b1cbf18
jorgevalladares <albertovr100@hotmail.com>
2022-03-23 18:51:09 -0600

    actualizando

--- Ejercicios/41 Ejercicio-50 Funciones Matematicas - tarea 8/main.cpp
+++ Ejercicios/41 Ejercicio-50 Funciones Matematicas - tarea 8/main.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-   double numero = 253;
+   double numero = 0;
    double raizcuadrada = sqrt(numero);
 
-   cout << "la raiz cuadrada de: " << numero << endl << raizcuadrada;
+   cout << "ingrese un numero para calcular su raiz" << endl;
+   cin >> numero;
 
-    
+   raizcuadrada = sqrt(numero);
+
+   cout << "la raiz cuadrada del numero ingresado es: " << raizcuadrada;
 
     return 0;
 }
